5618e861600bbf8a2b2112d95b5089f92627f1cf
Janick Kaltenmark <janick.kaltenmark@swr.de>
2023-04-07 20:02:06 +0200

    First website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { JerseyComponent } from './jersey/jersey.component';
+import { PlayerComponent } from './player/player.component';
+import { FieldsComponent } from './fields/fields.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    JerseyComponent,
+    PlayerComponent,
+    FieldsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
