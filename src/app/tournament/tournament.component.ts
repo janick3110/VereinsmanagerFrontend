88bdb87ddb699d06b37f7122f295920a654a94d7
janick3110 <janick3110@gmail.com>
2023-06-09 23:08:29 +0200

    Correct calculating of the table after the results (at least if we assume it's one group :D)

--- src/app/tournament/tournament.component.ts
+++ src/app/tournament/tournament.component.ts
@@ -24,6 +24,20 @@ interface Team{
   name: string;
 }
 
+interface Rank{
+  rank: number;
+  teamID: number;
+  games: number;
+  points: number;
+  goalsShot: number;
+  goalsConceded: number;
+  difference: number;
+}
+
+interface Table{
+  table: Rank[];
+}
+
 @Component({
   selector: 'app-tournament',
   templateUrl: './tournament.component.html',
@@ -32,12 +46,92 @@ interface Team{
 export class TournamentComponent {
   games: Game[] = [];
   teams: Team[] = [];
+  ranks: Rank[] = [];
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http.get<Game[]>('http://localhost:8080/tournament/tournaments/result/21/').subscribe(
       data => {
         this.games = data;
+        let integerList: number[] = []; // Your list of integers
+
+        for (const game of this.games) {
+          if (!integerList.includes(game.homeTeamID)) {
+            integerList.push(game.homeTeamID);
+          }
+
+          if (!integerList.includes(game.guestTeamID)) {
+            integerList.push(game.guestTeamID);
+          }
+        }
+
+
+
+        for (const team of integerList) {
+
+          let goalsShot = 0;
+          let goalsConceded = 0;
+          let points = 0;
+          let games = 0;
+
+          for (const game of this.games) {
+            if (game.played){
+              if (game.homeTeamID === team) {
+                goalsShot = goalsShot + game.homeGoals;
+                goalsConceded = goalsConceded + game.guestGoals;
+                points = points + game.pointsHome;
+                games = games + 1;
+              } else if (game.guestTeamID === team) {
+                goalsShot = goalsShot + game.guestGoals;
+                goalsConceded = goalsConceded + game.homeGoals;
+                points = points + game.pointsGuest;
+                games = games + 1;
+              }
+            }
+
+          }
+
+
+          let emptyRank: Rank = {
+            rank: 0,
+            teamID:0,
+            games:0,
+            points: 0,
+            goalsShot: 0,
+            goalsConceded: 0,
+            difference: 0
+          };
+
+          emptyRank.goalsShot = goalsShot;
+          emptyRank.goalsConceded = goalsConceded;
+          emptyRank.points = points;
+          emptyRank.teamID = team;
+          emptyRank.games = games;
+
+          this.ranks.push(emptyRank);
+        }
+
+
+        this.ranks.sort((a, b) => {
+          // Compare by points
+          if (b.points !== a.points) {
+            return b.points - a.points;
+          }
+
+          // Compare by difference
+          if (a.difference !== b.difference) {
+            return b.difference - a.difference;
+          }
+
+          // Compare by goalsShot
+          return b.goalsShot - a.goalsShot;
+        });
+
+        let index = 1;
+        for (const rank of this.ranks) {
+          rank.rank = index;
+          index = index + 1;
+        }
       },
       error => {
         console.error(error);
